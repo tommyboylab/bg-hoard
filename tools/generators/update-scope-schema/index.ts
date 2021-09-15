61dfde8502c444716e9f6e1ed05f5f789d945396
Thomas Fiala <thomasfiala@PB-0137-ThomasFiala.local>
2021-09-15 19:11:42 +0200

    fix: commit after generating in lab 14

--- tools/generators/update-scope-schema/index.ts
+++ tools/generators/update-scope-schema/index.ts
@@ -1,57 +1,54 @@
-import { Tree, updateJson, updateTs, readJson, formatFiles } from '@nrwl/devkit';
-import { libraryGenerator } from '@nrwl/workspace/generators';
+import { Tree, updateJson, formatFiles, readJson } from '@nrwl/devkit';
 
-export default async function (tree: Tree, schema: any) {
-  await updateJson(tree, 'workspace.json', (workspaceJson) => {
-  workspaceJson.defaultProject = 'api'
-  return workspaceJson
-  }
+function getScopes(nxJson: any) {
+  const projects: any[] = Object.values(nxJson.projects);
+  const allScopes: string[] = projects
+    .map((project) =>
+      project.tags.filter((tag: string) => tag.startsWith('scope:'))
+    )
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    .map((scope: string) => scope.slice(6));
+  return [...new Set(allScopes)];
+}
 
-  const projectList = readJson(tree, 'nx.json', (nxJson) => {
-  return getScopes(nxJson)
-  }
+function replaceScopes(content: string, scopes: string[]): string {
+  const joinScopes = scopes.map((s) => `'${s}'`).join(' | ');
+  const PATTERN = /interface Schema \{\n.*\n.*\n\}/gm;
+  return content.replace(
+    PATTERN,
+    `const Schema = {
+  name: string;
+  directory: ${joinScopes};
+}`
+  );
+}
 
-  await updateJson(tree, '/tools/util-lib/schema.json', (schemaJson) => {
-  schemaJson.properties.directory.items = projectList
+function addScopeIfMissing(tree: Tree) {
+  updateJson(tree, 'nx.json', (json) => {
+    Object.keys(json.projects).forEach((projectName) => {
+      if (
+        !json.projects[projectName].tags.some((tag) => tag.startsWith('scope:'))
+      ) {
+        const scope = projectName.split('-')[0];
+        json.projects[projectName].tags.push(`scope:${scope}`);
+      }
+    });
+    return json;
+  });
+}
 
-  return schemaJson
-  }
-
-  await updateTs(tree, '/tools/util-lib/index.ts', (indexTs) => {
-  return replaceScopes(indexTs)
-  }
-
+export default async function (tree: Tree) {
+  const scopes = getScopes(readJson(tree, 'nx.json'));
+  updateJson(tree, 'tools/generators/util-lib/schema.json', (schemaJson) => {
+    schemaJson.properties.directory['x-prompt'].items = scopes.map((scope) => ({
+      value: scope,
+      label: scope,
+    }));
+    return schemaJson;
+  });
+  const content = tree.read('tools/generators/util-lib/index.ts', 'utf-8');
+  const newContent = replaceScopes(content, scopes);
+  tree.write('tools/generators/util-lib/index.ts', newContent);
+  await addScopeIfMissing(tree)
   await formatFiles(tree);
-
-  function getScopes(nxJson: any) {
-    const projects: any[] = Object.values(nxJson.projects);
-    const allScopes: string[] = projects
-      .map((project) =>
-        project.tags
-          // take only those that point to scope
-          .filter((tag: string) => tag.startsWith('scope:'))
-      )
-      // flatten the array
-      .reduce((acc, tags) => [...acc, ...tags], [])
-      // remove prefix `scope:`
-      .map((scope: string) => scope.slice(6));
-    // remove duplicates
-    return Array.from(new Set(allScopes));
-  }
-
-  function replaceScopes(content: string, scopes: string[]): string {
-    const joinScopes = scopes.map((s) => `'${s}'`).join(' | ');
-    const PATTERN = /interface Schema \{\n.*\n.*\n\}/gm;
-    return content.replace(
-      PATTERN,
-      `const Schema = {
-    name: string;
-    directory: ${joinScopes};
-  }`
-    );
-  }
-
-  return () => {
-    installPackagesTask(tree);
-  };
 }
